edad57af616f9eb363b69f4a9db0f027832e8871
RozeGift <72850017+RozeGift@users.noreply.github.com>
2022-02-03 02:09:06 +0800

    -Code Added
    + Collision
    + Restart Function

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,15 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //coin collision
+            if(other.gameObject.tag == "Coin")
+            {
+                print("Coin");
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
